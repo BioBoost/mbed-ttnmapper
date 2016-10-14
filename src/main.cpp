ebe5f48e3ad8d77d72fecf363ded5b34db56adda
Sille Van Landschoot <info@sillevl.be>
2016-10-14 21:51:55 +0200

    cleanup main.cpp some more

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,3 @@
-/*
- / _____)             _              | |
-( (____  _____ ____ _| |_ _____  ____| |__
- \____ \| ___ |    (_   _) ___ |/ ___)  _ \
- _____) ) ____| | | || |_| ____( (___| | | |
-(______/|_____)_|_|_| \__)_____)\____)_| |_|
-    (C)2015 Semtech
-
-Description: MBED LoRaWAN example application
-
-License: Revised BSD License, see LICENSE.TXT file include in the project
-
-Maintainer: Miguel Luis and Gregory Cristian
-*/
 #include "mbed.h"
 
 DigitalOut txled(LED1);
@@ -46,19 +32,9 @@ int32_t randr( int32_t min, int32_t max )
 // MAIN - INITIALIZATION AND STARTUP
 //////////////////////////////////////////////////
 
-static void onRxLed( osjob_t* j )
-{
-    rxled = 0;
-}
-
 static void onTxLed( osjob_t* j )
 {
     txled = 0;
-}
-
-void processRxFrame( void )
-{
-
 }
 
 static void onSendFrame( osjob_t* j )
@@ -76,16 +52,10 @@ static void onInit( osjob_t* j )
     // reset MAC state
     LMIC_reset( );
     LMIC_setAdrMode( LORAWAN_ADR_ON );
-#if defined(CFG_eu868)
-    LMIC_setDrTxpow( DR_SF8, 14 );
-#elif defined(CFG_us915)
-    LMIC_setDrTxpow( DR_SF8, 14 );
-#endif
+    LMIC_setDrTxpow( DR_SF7, 14 );
 
     LMIC_setSession( LORAWAN_NET_ID, LORAWAN_DEV_ADDR, NwkSKey, ArtSKey );
     onSendFrame( NULL );
-
-    // init done - onEvent( ) callback will be invoked...
 }
 
 int main( void )
@@ -110,23 +80,8 @@ void onEvent( ev_t ev )
 
     switch( ev )
     {
-    // network joined, session established
-    case EV_JOINED:
-        txOn = true;
-        break;
     // scheduled data sent (optionally data received)
     case EV_TXCOMPLETE:
-        // Check if we have a downlink on either Rx1 or Rx2 windows
-        if( ( LMIC.txrxFlags & ( TXRX_DNW1 | TXRX_DNW2 ) ) != 0 )
-        {
-            rxled = 1;
-            os_setTimedCallback( &rxLedJob, os_getTime( ) + ms2osticks( 25 ), onRxLed );
-
-            if( LMIC.dataLen != 0 )
-            { // data received in rx slot after tx
-                processRxFrame( );
-            }
-        }
         txOn = true;
         break;
     default:
@@ -139,7 +94,7 @@ void onEvent( ev_t ev )
                              os_getTime( ) + ms2osticks( APP_TX_DUTYCYCLE + randr( -APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND ) ),
                              onSendFrame );
 
-        ////Sends frame as soon as possible (duty cylce limitations)
-        //onSendFrame( NULL );
+        // //Sends frame as soon as possible (duty cylce limitations)
+        // onSendFrame( NULL );
     }
 }
