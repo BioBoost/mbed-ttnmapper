e6b2954ee00ec548588fae67d15594ed2305d503
Sille Van Landschoot <info@sillevl.be>
2016-10-14 21:12:40 +0200

    lorawan example

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,282 @@
+/*
+ / _____)             _              | |
+( (____  _____ ____ _| |_ _____  ____| |__
+ \____ \| ___ |    (_   _) ___ |/ ___)  _ \
+ _____) ) ____| | | || |_| ____( (___| | | |
+(______/|_____)_|_|_| \__)_____)\____)_| |_|
+    (C)2015 Semtech
+
+Description: MBED LoRaWAN example application
+
+License: Revised BSD License, see LICENSE.TXT file include in the project
+
+Maintainer: Miguel Luis and Gregory Cristian
+*/
 #include "mbed.h"
-#include "Gps.h"
 
-Serial pc (USBTX, USBRX);
-Serial * gps_Serial;
-Gps* gps;
-Ticker gps_ticker;
+DigitalOut txled(LED1);
+DigitalOut rxled(LED2);
 
-void show_gps_info()
+#include "lmic.h"
+
+static uint8_t mydata[] = "Hello, world!";
+
+/*!
+ * When set to 1 the application uses the Over-the-Air activation procedure
+ * When set to 0 the application uses the Personalization activation procedure
+ */
+#define OVER_THE_AIR_ACTIVATION                     0
+
+#if( OVER_THE_AIR_ACTIVATION == 0 )
+
+/*!
+ * Defines the network ID when using personalization activation procedure
+ */
+#define LORAWAN_NET_ID                              ( uint32_t )0x00000000
+
+/*!
+ * Defines the device address when using personalization activation procedure
+ */
+#define LORAWAN_DEV_ADDR                            ( uint32_t )0x0D4991A1
+
+#endif
+
+/*!
+ * Defines the application data transmission duty cycle
+ */
+#define APP_TX_DUTYCYCLE                            5000 // 5 [s] value in ms
+#define APP_TX_DUTYCYCLE_RND                        1000 // 1 [s] value in ms
+
+/*!
+ * LoRaWAN Adaptative Data Rate
+ */
+#define LORAWAN_ADR_ON                              1
+
+/*!
+ * LoRaWAN confirmed messages
+ */
+#define LORAWAN_CONFIRMED_MSG_ON                    1
+
+/*!
+ * LoRaWAN application port
+ */
+#define LORAWAN_APP_PORT                            15
+
+/*!
+ * User application data buffer size
+ */
+#if ( LORAWAN_CONFIRMED_MSG_ON == 1 )
+#define LORAWAN_APP_DATA_SIZE                       6
+
+#else
+#define LORAWAN_APP_DATA_SIZE                       1
+
+#endif
+//////////////////////////////////////////////////
+// CONFIGURATION (FOR APPLICATION CALLBACKS BELOW)
+//////////////////////////////////////////////////
+
+// application router ID (LSBF)
+static const uint8_t AppEui[8] =
 {
-   //check if enough time has passed to warrant printing GPS info to screen
-   //note if refresh_Time is too low or pc.baud is too low, GPS data may be lost during printing
-   gps->debug(&pc);
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+// unique device ID (LSBF)
+static const u1_t DevEui[8] =
+{
+    0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF
+};
+
+// device-specific AES key (derived from device EUI)
+static const uint8_t DevKey[16] =
+{
+    0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6,
+    0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C
+};
+
+#if( OVER_THE_AIR_ACTIVATION == 0 )
+// network session key
+static uint8_t NwkSKey[] =
+{ 0xAC, 0xF7, 0xFD, 0xEA, 0xEE, 0x33, 0xF8, 0xD0, 0xA6, 0x80, 0x4A, 0xD0, 0x25, 0xC6, 0x41, 0x7B };
+
+// application session key
+static uint8_t ArtSKey[] =
+{ 0x29, 0x77, 0x31, 0x0A, 0x2B, 0x92, 0x35, 0xE1, 0x72, 0x96, 0x91, 0x39, 0xCC, 0xEB, 0x70, 0x66 };
+#endif
+
+// LEDs and Frame jobs
+osjob_t rxLedJob;
+osjob_t txLedJob;
+osjob_t sendFrameJob;
+
+// LED state
+static bool AppLedStateOn = false;
+
+//////////////////////////////////////////////////
+// Utility functions
+//////////////////////////////////////////////////
+/*!
+ * \brief Computes a random number between min and max
+ *
+ * \param [IN] min range minimum value
+ * \param [IN] max range maximum value
+ * \retval random random value in range min..max
+ */
+int32_t randr( int32_t min, int32_t max )
+{
+    return ( int32_t )rand( ) % ( max - min + 1 ) + min;
 }
 
-int main() {
+//////////////////////////////////////////////////
+// APPLICATION CALLBACKS
+//////////////////////////////////////////////////
 
-   pc.baud(115200); //sets virtual COM serial communication to high rate; this is to allow more time to be spent on GPS retrieval
-   gps_Serial = new Serial(p9,p10); //serial object for use w/ GPS
-   gps = new Gps(gps_Serial);
+// provide application router ID (8 bytes, LSBF)
+void os_getArtEui( uint8_t *buf )
+{
+    memcpy( buf, AppEui, 8 );
+}
 
-   pc.printf("Connection established at 115200 baud...\n");
+// provide device ID (8 bytes, LSBF)
+void os_getDevEui( uint8_t *buf )
+{
+    memcpy( buf, DevEui, 8 );
+}
 
-   wait(1);
+// provide device key (16 bytes)
+void os_getDevKey( uint8_t *buf )
+{
+    memcpy( buf, DevKey, 16 );
+}
 
-   gps_ticker.attach(&show_gps_info, 2.0);
+//////////////////////////////////////////////////
+// MAIN - INITIALIZATION AND STARTUP
+//////////////////////////////////////////////////
 
-   while(true){
-       gps->run();
-   }
+static void onRxLed( osjob_t* j )
+{
+    rxled = 0;
+}
+
+static void onTxLed( osjob_t* j )
+{
+    txled = 0;
+}
+
+static void prepareTxFrame( void )
+{
+    LMIC.frame[0] = AppLedStateOn;
+#if ( LORAWAN_CONFIRMED_MSG_ON == 1 )
+    LMIC.frame[1] = LMIC.seqnoDn >> 8;
+    LMIC.frame[2] = LMIC.seqnoDn;
+    LMIC.frame[3] = LMIC.rssi >> 8;
+    LMIC.frame[4] = LMIC.rssi;
+    LMIC.frame[5] = LMIC.snr;
+#endif
+}
+
+void processRxFrame( void )
+{
+    switch( LMIC.frame[LMIC.dataBeg - 1] ) // Check Rx port number
+    {
+        case 1: // The application LED can be controlled on port 1 or 2
+        case 2:
+            if( LMIC.dataLen == 1 )
+            {
+                AppLedStateOn = LMIC.frame[LMIC.dataBeg] & 0x01;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+static void onSendFrame( osjob_t* j )
+{
+    prepareTxFrame( );
+    //LMIC_setTxData2( LORAWAN_APP_PORT, LMIC.frame, LORAWAN_APP_DATA_SIZE, LORAWAN_CONFIRMED_MSG_ON );
+    LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+
+    // Blink Tx LED
+    txled = 1;
+    os_setTimedCallback( &txLedJob, os_getTime( ) + ms2osticks( 25 ), onTxLed );
+}
+
+// Initialization job
+static void onInit( osjob_t* j )
+{
+    // reset MAC state
+    LMIC_reset( );
+    LMIC_setAdrMode( LORAWAN_ADR_ON );
+#if defined(CFG_eu868)
+    LMIC_setDrTxpow( DR_SF8, 14 );
+#elif defined(CFG_us915)
+    LMIC_setDrTxpow( DR_SF8, 14 );
+#endif
+
+    // start joining
+#if( OVER_THE_AIR_ACTIVATION != 0 )
+    LMIC_startJoining( );
+#else
+    LMIC_setSession( LORAWAN_NET_ID, LORAWAN_DEV_ADDR, NwkSKey, ArtSKey );
+    onSendFrame( NULL );
+#endif
+    // init done - onEvent( ) callback will be invoked...
+}
+
+int main( void )
+{
+    osjob_t initjob;
+
+    // initialize runtime env
+    os_init( );
+    // setup initial job
+    os_setCallback( &initjob, onInit );
+    // execute scheduled jobs and events
+    os_runloop( );
+    // (not reached)
+}
+
+//////////////////////////////////////////////////
+// LMIC EVENT CALLBACK
+//////////////////////////////////////////////////
+void onEvent( ev_t ev )
+{
+    bool txOn = false;
+
+    switch( ev )
+    {
+    // network joined, session established
+    case EV_JOINED:
+        txOn = true;
+        break;
+    // scheduled data sent (optionally data received)
+    case EV_TXCOMPLETE:
+        // Check if we have a downlink on either Rx1 or Rx2 windows
+        if( ( LMIC.txrxFlags & ( TXRX_DNW1 | TXRX_DNW2 ) ) != 0 )
+        {
+            rxled = 1;
+            os_setTimedCallback( &rxLedJob, os_getTime( ) + ms2osticks( 25 ), onRxLed );
+
+            if( LMIC.dataLen != 0 )
+            { // data received in rx slot after tx
+                processRxFrame( );
+            }
+        }
+        txOn = true;
+        break;
+    default:
+        break;
+    }
+    if( txOn == true )
+    {
+        //Sends frame every APP_TX_DUTYCYCLE +/- APP_TX_DUTYCYCLE_RND random time (if not duty cycle limited)
+        os_setTimedCallback( &sendFrameJob,
+                             os_getTime( ) + ms2osticks( APP_TX_DUTYCYCLE + randr( -APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND ) ),
+                             onSendFrame );
+
+        ////Sends frame as soon as possible (duty cylce limitations)
+        //onSendFrame( NULL );
+    }
 }
