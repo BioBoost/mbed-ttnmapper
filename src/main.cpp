a994c48dd9dd66033479a4287dc40eb1db16ddb3
Sille Van Landschoot <info@sillevl.be>
2016-10-16 07:44:07 +0200

    moved packet generation to Packet class

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Simple-LoRaWAN.h"
 #include "Gps.h"
 #include "settings.h"
+#include "Packet.h"
 
 using namespace SimpleLoRaWAN;
 
@@ -16,55 +17,13 @@ DigitalOut tx_led(LED1);
 
 Node* node;
 
-uint8_t txBuffer[9];
-
-void build_packet(Gps* gps)
-{
-    uint32_t LatitudeBinary, LongitudeBinary;
-    uint16_t altitudeGps;
-    uint8_t hdopGps;
-
-    LatitudeBinary = ((gps->latitude_in_degrees() + 90) / 180.0) * 16777215;
-    LongitudeBinary = ((gps->longitude_in_degrees() + 180) / 360.0) * 16777215;
-
-    txBuffer[0] = ( LatitudeBinary >> 16 ) & 0xFF;
-    txBuffer[1] = ( LatitudeBinary >> 8 ) & 0xFF;
-    txBuffer[2] = LatitudeBinary & 0xFF;
-
-    txBuffer[3] = ( LongitudeBinary >> 16 ) & 0xFF;
-    txBuffer[4] = ( LongitudeBinary >> 8 ) & 0xFF;
-    txBuffer[5] = LongitudeBinary & 0xFF;
-
-    altitudeGps = (int) gps->altitude;
-    txBuffer[6] = ( altitudeGps >> 8 ) & 0xFF;
-    txBuffer[7] = altitudeGps & 0xFF;
-
-    hdopGps = (int)gps->HDOP*10.0;
-    txBuffer[8] = hdopGps & 0xFF;
-
-    /*
-    Time: 14:52:13.0
-    Date: 15/10/2016
-    Fix: 1
-    Quality: 2
-    Location: 51.173592N, 3.217686E
-    HDOP:  0.87
-    Speed:  0.00 knots
-    Angle: 302.56
-    Altitude:  6.80
-    Satellites: 10
-    {0xc8, 0xc7, 0xbb, 0x82, 0x49, 0xc2, 0x00, 0x06, 0x00}
-    */
-
-}
-
 void debug_tx_buffer()
 {
-    for(int i = 0; i<sizeof(txBuffer); i++)
-    {
-        pc.printf("0x%02x ", txBuffer[i]);
-    }
-    pc.printf("\r\n");
+    // for(int i = 0; i<sizeof(txBuffer); i++)
+    // {
+    //     pc.printf("0x%02x ", txBuffer[i]);
+    // }
+    // pc.printf("\r\n");
 }
 
 void show_gps_info()
@@ -76,9 +35,9 @@ void send_gps_info()
 {
     if(gps->fix){
         pc.printf("Sending packet:");
-        build_packet(gps);
-        debug_tx_buffer();
-        node->send(reinterpret_cast<char*>(txBuffer), 9);
+        uint8_t* packet = Packet::build(gps);
+        //debug_tx_buffer();
+        node->send(reinterpret_cast<char*>(packet), 9);
     }
 }
 
